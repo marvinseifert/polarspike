f19e0b12544094f309904481f20996b802338710
marvinseifert <40001645+marvinseifert@users.noreply.github.com>
2024-01-19 17:36:23 +0000

    added quality_index.ipynb example

--- plotly_templates.py
+++ plotly_templates.py
@@ -42,7 +42,7 @@ pio.templates["scatter_template_black"] = go.layout.Template(
             tickfont=dict(family="Arial", color="white"),
             titlefont=dict(family="Arial", color="white"),
             showgrid=False,
-            title=dict(standoff=1)
+            title=dict(standoff=1),
         ),
         yaxis=dict(
             showline=True,
@@ -52,7 +52,7 @@ pio.templates["scatter_template_black"] = go.layout.Template(
             tickfont=dict(family="Arial", color="white"),
             titlefont=dict(family="Arial", color="white"),
             showgrid=False,
-            title=dict(standoff=1)
+            title=dict(standoff=1),
         ),
         plot_bgcolor="rgba(0, 0, 0, 255)",
         paper_bgcolor="rgba(0, 0, 0, 255)",
@@ -84,5 +84,36 @@ pio.templates["scatter_template_jupyter"] = go.layout.Template(
         paper_bgcolor="rgba(0, 0, 0, 0)",
         margin_pad=10,
         colorway=px.colors.qualitative.Dark2,
+    )
+)
+
+
+pio.templates["bar_template"] = go.layout.Template(
+    layout=go.Layout(
+        xaxis=dict(
+            showline=True,
+            linewidth=2,
+            linecolor="black",
+            ticks="outside",
+            tickfont=dict(family="Arial"),
+            titlefont=dict(family="Arial"),
+            showgrid=False,
+            zeroline=False,
+            title=dict(standoff=1),
+        ),
+        yaxis=dict(
+            showline=True,
+            linewidth=2,
+            linecolor="black",
+            ticks="outside",
+            tickfont=dict(family="Arial"),
+            titlefont=dict(family="Arial"),
+            showgrid=False,
+            zeroline=False,
+            title=dict(standoff=1),
+        ),
+        plot_bgcolor="rgba(255, 255, 255, 255)",
+        paper_bgcolor="rgba(255, 255, 255, 255)",
+        margin_pad=0,
     )
 )
